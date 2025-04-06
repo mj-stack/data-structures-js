1f69d0ba8b28294e2f7b69b8eb4f6858fac77e34
Mukul Joshi <mjoshi1819@gmail.com>
2025-04-06 18:23:51 +0530

    Implement traverse and deleteByValue methods in LinkedList class

--- 04-linked-list/linkedList.js
+++ 04-linked-list/linkedList.js
@@ -22,6 +22,39 @@ class LinkedList {
       current.next = newNode;
     }
   }
+
+  traverse() {
+    if (this.head === null) {
+      return;
+    }
+
+    let current = this.head;
+    console.log(current.data);
+
+    while (current.next !== null) {
+      current = current.next;
+      console.log(current.data);
+    }
+  }
+
+  deleteByValue(dataToDelete) {
+    if (this.head === null) {
+      return;
+    }
+
+    let current = this.head;
+    if (current.data === dataToDelete) {
+      this.head = current.next;
+      return;
+    }
+  }
 }
+
+const linkedList = new LinkedList();
+linkedList.insertAtEnd(2);
+linkedList.insertAtEnd(3);
+linkedList.insertAtEnd(4);
+linkedList.deleteByValue(3);
+linkedList.traverse();
 
 export default Node;
